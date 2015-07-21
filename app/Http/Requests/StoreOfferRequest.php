d9c4813cec0c0804af34fc6c37a13336f080ab84
Andrew Golubev <aagolubev@gmail.com>
2015-07-21 18:44:52 +0300

    Added Recaptcha to all relevant forms.
    Also fixed validation mistake.

--- app/Http/Requests/StoreOfferRequest.php
+++ app/Http/Requests/StoreOfferRequest.php
@@ -23,15 +23,15 @@ class StoreOfferRequest extends Request {
 	public function rules()
 	{
 		return [
-			'sell_user_ign'            => 'required|max:255|alpha_num_dots',
-			'sell_item_platform'       => 'required|max:8|alpha_num_spaces',
-			'sell_item_name'           => 'required|max:255|alpha_num_spaces',
-			'sell_item_rank'           => 'required|max:11|numeric',
-			'sell_item_price'          => 'required|max:1000|min:1|numeric',
-			'sell_item_commentary'     => 'max:250',
-			'wax_name'                 => 'honeypot',
-			'wax_time'                 => 'required|honeytime:3',
-			'recaptcha_response_field' => 'recaptcha',
+			'sell_user_ign'        => 'required|max:255|alpha_num_dots',
+			'sell_item_platform'   => 'required|max:8|alpha_num_spaces',
+			'sell_item_name'       => 'required|max:255|alpha_num_spaces',
+			'sell_item_rank'       => 'required|max:11|numeric',
+			'sell_item_price'      => 'required|max:1000|min:1|numeric',
+			'sell_item_commentary' => 'max:250',
+			'wax_name'             => 'honeypot',
+			'wax_time'             => 'required|honeytime:3',
+			'g-recaptcha-response' => 'required|recaptcha',
 		];
 	}
 
